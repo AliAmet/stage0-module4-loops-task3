9412b079888a97a9441d0d9084e33f2edf3a76e7
AliAmet <ametov1ali@gmail.com>
2023-10-25 19:35:30 +0600

    first commit

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -3,7 +3,9 @@ package school.mjc.stage0.loops.task3;
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
         int max = 0;
-        if (first >= second) {
+        if (first == 0 || second == 0)
+            max = first>second? first:second;
+        else if (first >= second) {
             for (int i = 1; i <= second; i++){
                 if (first % i == 0 && second % i == 0)
                     max = i;
